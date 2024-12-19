5377c28fb8a9a3aaa5ebe33ddf9aff464a7dd073
Paper Engineer <trinhvutuanhung@gmail.com>
2024-12-19 14:49:40 +0700

    Updated

--- RevitAPI.extension/PapeEnginner.tab/About.panel/Test_case_2.pushbutton/script.py
+++ RevitAPI.extension/PapeEnginner.tab/About.panel/Test_case_2.pushbutton/script.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 # TODO: import library
 import clr  # Common Language Runtime for .NET
 import System
@@ -44,62 +43,72 @@ unit = doc.GetUnits()
 version = int(app.VersionNumber)
 selection = uidoc.Selection
 
-uiviews = uidoc.GetOpenUIViews()
-uiview = [x for x in uiviews if x.ViewId == view.Id][0]
-
-# ------Note: __revit__ = Autodesk.Revit.UI.UIApplication
 """----------------------FUNCTION----------------------------"""
+
+
+def SetWorkset(askWorkset, ele):
+    """
+    Đặt workset cho một element cụ thể
+    """
+    # Lấy danh sách tất cả Workset
+    worksetCollector = FilteredWorksetCollector(doc).OfKind(WorksetKind.UserWorkset).ToWorksets()
+    selectedWorksetId = None
+
+    # Tìm Workset ID dựa trên tên được chọn
+    for workset in worksetCollector:
+        if workset.Name == askWorkset:
+            selectedWorksetId = workset.Id.IntegerValue
+            break
+
+    # Nếu không tìm thấy Workset, thông báo lỗi
+    if selectedWorksetId is None:
+        TaskDialog.Show("Error", "Workset '{}' not found.".format(askWorkset))
+        return
+
+    # Bắt đầu giao dịch để đặt Workset cho element
+    with Transaction(doc, 'Set Workset') as t:
+        t.Start()
+        param = ele.get_Parameter(BuiltInParameter.ELEM_PARTITION_PARAM)
+
+        if param and param.IsReadOnly == False:
+            param.Set(selectedWorksetId)
+        else:
+            TaskDialog.Show("Error", "Cannot set Workset for element ID: {}".format(ele.Id))
+        t.Commit()
+
 
 """----------------------MAIN CODE----------------------------"""
 
 try:
-    refEle = uidoc.Selection.PickObjects(ObjectType.Element, "Change Workset")
-    ele = [doc.GetElement(ref.ElementId) for ref in refEle]
+    # Người dùng chọn các phần tử trong mô hình
+    refEle = uidoc.Selection.PickObjects(ObjectType.Element, "Pick Objects")
+    getEle = [doc.GetElement(e.ElementId) for e in refEle]
 
+    # Lấy danh sách tất cả Workset
     worksetCollector = FilteredWorksetCollector(doc).OfKind(WorksetKind.UserWorkset).ToWorksets()
+    worksetName = [i.Name for i in worksetCollector]
 
-    if worksetCollector is None:
-        TaskDialog.Show("Warning", "Not a working share file")
+    # Kiểm tra nếu không có Workset nào
+    if not worksetName:
+        TaskDialog.Show("Error", "No worksets available in the model.")
     else:
-        worksetName = [i.Name for i in worksetCollector]
-        worksetId = [i.Id.IntegerValue for i in worksetCollector]
-
-        dictWorkset = dict(zip(worksetName, worksetId))
-
-        selectedWorksetName = forms.ask_for_one_item(
+        # Yêu cầu người dùng chọn Workset
+        askWorkset = forms.ask_for_one_item(
             worksetName,
-            default=worksetName[0],
+            default=worksetName[0] if worksetName else None,
             prompt='Choose Workset',
-            title='Select Workset')
-
-        worksetIdChoose = dictWorkset.get(selectedWorksetName)
-
-        with TransactionGroup(doc, "Set Workset") as tg:
+            title='Please select a workset'
+        )
+        # Áp dụng Workset cho từng phần tử đã chọn
+        with TransactionGroup(doc,'Set Workset for All') as tg:
             tg.Start()
-            for e in ele:
-                worksetPara = e.get_Parameter(BuiltInParameter.ELEM_PARTITION_PARAM)
-                worksetPara.Set(worksetIdChoose)
-                tg.Assimilate()
-            tg.RollBack()
-            TaskDialog.Show("Success","Done")
-
-
-
-
-
-
-
-
-
-
-
-
-
+            for e in getEle:
+                SetWorkset(askWorkset, e)
+            tg.Assimilate()
 
 # Handle the case when the user cancels the operation
 except Autodesk.Revit.Exceptions.OperationCanceledException:
     pass
-
 
 # Handle any other exceptions and show an error message
 except Exception as ex:
