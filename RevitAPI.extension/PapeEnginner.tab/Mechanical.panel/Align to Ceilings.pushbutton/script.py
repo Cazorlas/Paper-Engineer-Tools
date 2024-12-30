ff4ee5b862319b26692625a8c6dbf98b2a8e6a00
Paper Engineer <trinhvutuanhung@gmail.com>
2024-12-30 09:23:36 +0700

    Updated

---
+++ RevitAPI.extension/PapeEnginner.tab/Mechanical.panel/Align to Ceilings.pushbutton/script.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import clr  # Common Language Runtime for .NET
+import System
+import math  # Standard Python math library
+# from MainForm import MainForm
+
+# Import necessary .NET and Revit API libraries
+from System.Collections.Generic import *
+from pyrevit import forms, revit, script
+
+clr.AddReference('ProtoGeometry')  # Dynamo's geometry proxy
+from Autodesk.DesignScript.Geometry import *  # Import everything from Dynamo's geometry
+
+clr.AddReference("RevitAPI")  # Revit API DLLs
+clr.AddReference("RevitAPIUI")  # Revit UI DLLs
+
+from MainForm import *
+
+import Autodesk
+from Autodesk.Revit.DB import *  # Revit API classes
+from Autodesk.Revit.UI import *  # Revit UI classes
+from Autodesk.Revit.UI.Selection import *  # For handling Revit selections
+from Autodesk.Revit.DB.Mechanical import Duct, MechanicalUtils
+from Autodesk.Revit.DB.Plumbing import Pipe, PlumbingUtils
+
+clr.AddReference("RevitNodes")  # Dynamo nodes for Revit
+import Revit  # Import Revit namespace in RevitNodes
+
+clr.ImportExtensions(Revit.Elements)
+clr.ImportExtensions(Revit.GeometryConversion)
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager  # Document management in Revit
+from RevitServices.Transactions import TransactionManager  # Transaction management
+
+# Prepare document and other variables
+doc = __revit__.ActiveUIDocument.Document
+view = doc.ActiveView
+uidoc = __revit__.ActiveUIDocument
+app = __revit__.Application
+DB = Autodesk.Revit.DB
+output = script.get_output()
+unit = doc.GetUnits()
+version = int(app.VersionNumber)
+
+""" ----------------------FUNCTIONS----------------------------"""
+
+
+
+
+
+"""----------------------MAIN CODE----------------------------"""
+try:
+
+
+
+
+except Autodesk.Revit.Exceptions.OperationCanceledException:
+    pass
+
+except Exception as ex:
+    TaskDialog.Show("Error", "Warning: {}".format(ex))  # Corrected string formatting
